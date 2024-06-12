9e0d98750141d5972c1556f98a7e771c6b9710e2
Andrii Shapovalov <ashxdev@gmail.com>
2024-06-12 20:04:47 +0300

    delete base patch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ashxdev.github.io',
-    base: '/trystavisim',
+    site: 'https://ashxdev.github.io'
 });
